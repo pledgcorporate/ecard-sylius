7858e3c9c1ddbc187d38785e94ced6941687d64f
Etienne GASCHET <etienne.gaschet@pledg.co>
2026-05-13 14:11:39 +0200

    clean dev enviromnent

--- tests/Application/config/sylius/1.11/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
-    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
-    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-];
