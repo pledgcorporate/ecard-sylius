f073522648680acc0196d31d14fc195ae103cce7
Etienne GASCHET <etienne.gaschet@pledg.co>
2026-06-02 14:47:44 +0200

    admin: sandbox checkbox first in form

--- src/Form/Type/PledgConfigurationType.php
+++ src/Form/Type/PledgConfigurationType.php
@@ -17,38 +17,20 @@ class PledgConfigurationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add(PledgGatewayFactory::SANDBOX, CheckboxType::class, [
+                'label' => 'pledg_sylius_payment_plugin.sandbox',
+                'required' => false,
+                'help' => 'pledg_sylius_payment_plugin.sandbox_help',
+            ])
             ->add(PledgGatewayFactory::IDENTIFIER, TextType::class, [
                 'label' => 'pledg_sylius_payment_plugin.identifier',
             ])
             ->add(PledgGatewayFactory::SECRET, TextType::class, [
                 'label' => 'pledg_sylius_payment_plugin.secret',
             ])
-            ->add(PledgGatewayFactory::API_KEY, TextType::class, [
-                'label' => 'pledg_sylius_payment_plugin.api_key',
-                'required' => false,
-                'help' => 'pledg_sylius_payment_plugin.api_key_help',
-            ])
-            ->add(PledgGatewayFactory::API_SECRET, TextType::class, [
-                'label' => 'pledg_sylius_payment_plugin.api_secret',
-                'required' => false,
-                'help' => 'pledg_sylius_payment_plugin.api_secret_help',
-                'attr' => ['autocomplete' => 'new-password'],
-            ])
-            ->add(PledgGatewayFactory::COMPANY_UID, TextType::class, [
-                'label' => 'pledg_sylius_payment_plugin.company_uid',
-                'required' => false,
-                'help' => 'pledg_sylius_payment_plugin.company_uid_help',
-            ])
-            ->add(PledgGatewayFactory::WIDGET_PRODUCT_ENABLED, CheckboxType::class, [
-                'label' => 'pledg_sylius_payment_plugin.widget_product',
-                'required' => false,
-            ])
-            ->add(PledgGatewayFactory::WIDGET_CHECKOUT_ENABLED, CheckboxType::class, [
-                'label' => 'pledg_sylius_payment_plugin.widget_checkout',
-                'required' => false,
-            ])
-            ->add(PledgGatewayFactory::WIDGET_CATALOG_ENABLED, CheckboxType::class, [
-                'label' => 'pledg_sylius_payment_plugin.widget_catalog',
+            ->add(PledgGatewayFactory::RESTRICTED_COUNTRIES, CountryType::class, [
+                'label' => 'pledg_sylius_payment_plugin.restricted_countries',
+                'multiple' => true,
                 'required' => false,
             ])
             ->add(PledgGatewayFactory::PRICE_RANGE_MIN, NumberType::class, [
@@ -61,15 +43,43 @@ class PledgConfigurationType extends AbstractType
                 'required' => false,
                 'scale' => 0,
             ])
-            ->add(PledgGatewayFactory::RESTRICTED_COUNTRIES, CountryType::class, [
-                'label' => 'pledg_sylius_payment_plugin.restricted_countries',
-                'multiple' => true,
+            // for now, the 3 fields below are not used and are hidden with class="d-none"
+            ->add(PledgGatewayFactory::API_KEY, TextType::class, [
+                'label' => 'pledg_sylius_payment_plugin.api_key',
+                'required' => false,
+                'help' => 'pledg_sylius_payment_plugin.api_key_help',
+                "row_attr" => [
+                    "class" => "d-none"
+                ],
+            ])
+            ->add(PledgGatewayFactory::API_SECRET, TextType::class, [
+                'label' => 'pledg_sylius_payment_plugin.api_secret',
+                'required' => false,
+                'help' => 'pledg_sylius_payment_plugin.api_secret_help',
+                'attr' => ['autocomplete' => 'new-password'],
+                "row_attr" => [
+                    "class" => "d-none"
+                ],
+            ])
+            ->add(PledgGatewayFactory::COMPANY_UID, TextType::class, [
+                'label' => 'pledg_sylius_payment_plugin.company_uid',
+                'required' => false,
+                'help' => 'pledg_sylius_payment_plugin.company_uid_help',
+                "row_attr" => [
+                    "class" => "d-none"
+                ],
+            ])
+            ->add(PledgGatewayFactory::WIDGET_PRODUCT_ENABLED, CheckboxType::class, [
+                'label' => 'pledg_sylius_payment_plugin.widget_product',
                 'required' => false,
             ])
-            ->add(PledgGatewayFactory::SANDBOX, CheckboxType::class, [
-                'label' => 'pledg_sylius_payment_plugin.sandbox',
+            ->add(PledgGatewayFactory::WIDGET_CHECKOUT_ENABLED, CheckboxType::class, [
+                'label' => 'pledg_sylius_payment_plugin.widget_checkout',
                 'required' => false,
-                'help' => 'pledg_sylius_payment_plugin.sandbox_help',
+            ])
+            ->add(PledgGatewayFactory::WIDGET_CATALOG_ENABLED, CheckboxType::class, [
+                'label' => 'pledg_sylius_payment_plugin.widget_catalog',
+                'required' => false,
             ])
         ;
     }
